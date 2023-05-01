e475d91ba87962da039916c933b583abe91565a9
houssinCarnelian <houssin.carnelian@gmail.com>
2023-05-01 15:23:36 +0100

    installing multter

--- controllers/postCltr.js
+++ controllers/postCltr.js
@@ -1,9 +1,13 @@
 const { Post } = require('../model/posteModel.js')
 exports.createPostCrtl = async (req, res) => {
+    const {title,content,meta,tags,slug,author} = req.body
     const newpost = await Post.create({
-        title: req.body.title,
-        content: req.body.content,
-        meta: req.body.meta
+        title: title,
+        content: content,
+        meta: meta,
+        tags: tags,
+        slug: slug,
+        author: author
     })
     res.status(201).json(newpost)
 }
