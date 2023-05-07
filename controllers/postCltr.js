f7f78e0d243cb6b4650fed42abff370fb66266cb
houssinCarnelian <houssin.carnelian@gmail.com>
2023-05-07 13:39:06 +0100

    get all get count get by id

--- controllers/postCltr.js
+++ controllers/postCltr.js
@@ -1,6 +1,6 @@
 const { Post } = require('../model/posteModel.js')
 exports.createPostCrtl = async (req, res) => {
-    const {title,content,meta,tags,slug,author} = req.body
+    const { title, content, meta, tags, slug, author } = req.body
     console.log(req.file);
     const newpost = await Post.create({
         title: title,
@@ -14,3 +14,30 @@ exports.createPostCrtl = async (req, res) => {
 }
 
 //5.send response to the client
+
+exports.postCountCltr = async (req, res) => {
+    const postCount = await Post.count()
+    res.status(200).json(postCount)
+}
+exports.getAllPostsCltr = async (req, res) => {
+    const POST_PER_PAGE = 3
+    const { PageNumber } = req.query
+    let AllPsots
+
+    if (PageNumber) {
+        AllPsots = await Post.find()
+            .skip((PageNumber - 1) * POST_PER_PAGE)
+            .limit(POST_PER_PAGE)
+            .sort({ createdAt: -1 })
+        res.status(200).json(AllPsots)
+    } else {
+        AllPsots = await Post.find()
+            .sort({ createdAt: -1 })
+        res.status(200).json(AllPsots)
+    }
+
+}
+exports.getPostsByIdCltr = async (req, res) => {
+    const Singlepost = await Post.findById(req.params.id)
+    res.status(200).json(Singlepost)
+}
--- routes/post.js
+++ routes/post.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-const { createPostCrtl } = require('../controllers/postCltr.js');
+const { createPostCrtl, postCountCltr,getAllPostsCltr,getPostsByIdCltr } = require('../controllers/postCltr.js');
 const photoUpload = require('../middlewares/photoUpload.js');
 
-router.post('/create',photoUpload.single('thumbnail'),createPostCrtl)
-
+router.route('/').get(getAllPostsCltr)
+router.post('/create', photoUpload.single('thumbnail'), createPostCrtl)
+router.route('/count').get(postCountCltr)
+router.route('/:id').get(getPostsByIdCltr)
 module.exports = router
